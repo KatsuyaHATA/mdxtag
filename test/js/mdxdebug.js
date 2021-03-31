a98861b18046bda313c70af164e6aa65640ea727
KatsuyaHATA <50607092+KatsuyaHATA@users.noreply.github.com>
2021-03-31 21:05:53 +0900

    Add files via upload

--- test/js/mdxdebug.js
+++ test/js/mdxdebug.js
@@ -35,14 +35,27 @@ mdxurltest = {
         }
         __add_params = p;
     },
+    send_params: function(p) {
+        if (!window.location.search) {
+            console.log("mdxurltest.send_params : Query parameter is not exist");
+        }
+        __send_params = p;
+    },
     enable_hash: function(flg) {
         if (!window.location.hash) {
             console.log("mdxurltest.enable_hash : hash is not exist");
         }
         __enable_url_hash = flg||0;
     },
+    custom_parameter: function(a, c) {
+        __action = a||"";
+        __custom_param = c||{};
+    },
     clear: function() {
         __add_params = [];
+        __send_params = [];
+        __custom_param = {};
+        __action = "";
         __enable_url_hash = 0;
     }
 };
@@ -55,6 +68,22 @@ jQuery(document).ready(function() {
         mdxurltest.add_params(p);
         val = jQuery("input[name='enable_hash']:checked").val();
         mdxurltest.enable_hash((+(val)));
+        __push_beacon(__getURL());
+        mdxurltest.clear();
+    });
+    jQuery("button#parameter-test").click(function(e) {
+        var val = jQuery("input[name='sned_params']").val();
+        var p = val.replace(/(^\s*|\s*$)/g,'').split(/\s*,\s*/);
+        mdxurltest.send_params(p);
+        val = jQuery("[name='custom_param']").val();
+        try {
+            p = JSON.parse(val);
+        } catch(e) {
+            console.log(e);
+            return;
+        }
+        var act = jQuery("input[name='action']").val();
+        mdxurltest.custom_parameter(act, p);
         __push_beacon(__getURL());
         mdxurltest.clear();
     });
